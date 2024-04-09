9a0e6f5e776ee69fbe6c069b8fe9b5da9fa97646
Kaleb Takele <kaleb.t@kachadfs.com>
2024-04-09 14:55:29 +0300

    Initial commit

---
+++ apache-camel-consumer/src/main/java/test/kaleb/apachecamelconsumer/ApacheCamelConsumerApplication.java
@@ -0,0 +1,13 @@
+package test.kaleb.apachecamelconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelConsumerApplication.class, args);
+	}
+
+}
